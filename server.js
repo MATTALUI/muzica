25b53a335dc877369978388c3e4f5f2f568e672d
Matthew Hummer <biggreyson@gmail.com>
2017-06-09 16:12:39 -0600

    add server skeleton

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+
+app.use(morgan());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get('/', (req, res, next)=>{
+  res.send('hello world');
+})
+
+
+
+app.listen(port, ()=>{
+  console.log('listening on ', port);
+});
