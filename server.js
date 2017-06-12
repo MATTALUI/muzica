ca331992f0b8f5ac43d1ec83aa455114a82d225e
Brennen Barret Bull <brennenbull@Brennens-MacBook-Pro.local>
2017-06-11 20:18:25 -0600

    add middleware for CORS

--- server.js
+++ server.js
@@ -16,10 +16,15 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/dbapi', dbapi);
 app.get('/', (req, res, next)=>{
   res.send('hello world');
-})
+});
 app.use('/', (req,res,next)=>{
   res.sendStatus(404);
 });
