873207539b67414df514da10f1ef8687e4779a74
Matthew Hummer <biggreyson@gmail.com>
2017-06-12 18:42:25 -0600

    add users route

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex');
+// const humps = require('humps');
+
+router.post('/login', (req,res,next)=>{
+  res.send(req.body)
+});
+
+
+
+module.exports = router;
