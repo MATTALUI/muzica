80087bc8b6602f69f0e08c695ecd3c98f3cd7953
Adam Smith <asmith13194@gmail.com>
2017-06-12 18:59:13 -0600

    user login post

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
+const bcrypt = require('bcrypt');
 // const humps = require('humps');
 
 router.post('/login', (req,res,next)=>{
