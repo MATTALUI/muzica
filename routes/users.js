5fe9b84a1a6939549c5f274383818abfe64aff7b
gpciancio <gopalciancio@gmail.com>
2017-06-13 10:34:43 -0600

    modal updated

--- routes/users.js
+++ routes/users.js
@@ -3,35 +3,46 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 const bcrypt = require('bcrypt');
-var path    = require("path");
+const path = require("path");
+const jwt = require('jsonwebtoken');
 // const humps = require('humps');
 
 router.post('/login', (req, res, next) => {
-  console.log(req.body);
+  // console.log(req.body);
   knex('users')
     .select('*')
     .where('email', req.body.email)
     .first()
     .then(function(user) {
-      console.log(user);
+      // console.log(user);
       if (user.length === 0) {
         res.setHeader('Content-Type', 'text/plain');
         alert("Incorrect email or password");
       } else {
         bcrypt.compare(req.body.password, user.hashed_password, function(err, match) {
-          console.log(req.body);
+          // console.log(req.body);
           if (err) {
             alert('Invalid email or password')
           } else {
-            res.send('Valid email and password')
-            // res.sendFile(path.join( __dirname+'/home.html'));
+            var token = jwt.sign(user, 'secret');
+            // console.log(token);
+            res.cookie('token', token, {
+              httpOnly: true
+            });
+            // console.log(req.cookies);
+            console.log(user.id);
+            // knex('projects')
+            //   .select('*')
+            //   .where('user_id', 'user.id')
+            //   .then(projects => {
+            //     console.log(projects);
+                res.send('Valid email and password')
+            //     // res.sendFile(path.join( __dirname+'/home.html'));
+            //   })
           }
         });
       }
-
     });
-
-
 });
 
 
