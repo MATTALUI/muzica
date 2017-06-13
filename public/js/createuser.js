ec7e15ad3714fb348d9ed0720a6f368bd2689a9e
gpciancio <gopalciancio@gmail.com>
2017-06-13 17:15:24 -0600

    fix confirm pass

--- public/js/createuser.js
+++ public/js/createuser.js
@@ -5,7 +5,7 @@ $('#create_user').on ('click', ()=>{
   var first_name = $("#first_name").val();
   var last_name = $("#last_name").val();
   var sc_username = $("#sc_username").val();
-  var confirm_pass = $('#confirm_password')
+  var confirm_pass = $('#confirm_password').val()
 
   if (pass!==confirm_pass) {
     console.log('passwords do not match');
@@ -19,7 +19,6 @@ $('#create_user').on ('click', ()=>{
       email: email,
       password: pass,
       first_name: first_name,
-      last_name: last_name,
       sc_username: sc_username
     },
     success: function(res){
