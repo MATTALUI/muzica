43112b66cdcddb41b81b7217ecbf974e16243450
Matthew Hummer <biggreyson@gmail.com>
2017-06-14 17:17:23 -0600

    add delete project route

--- migrations/20170613111419_commits.js
+++ migrations/20170613111419_commits.js
@@ -1,8 +1,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("commits", function (table) {
   table.increments('id');
-  table.integer('project_id').index().references('projects.id').notNullable();
-  table.integer('submitted_by').index().references('users.id').notNullable();
+  table.integer('project_id').index().references('projects.id').notNullable().onDelete('cascade');
+  table.integer('submitted_by').index().references('users.id').notNullable().onDelete('cascade');
   table.string('widget_url').notNullable().default('https://w.soundcloud.com/player/?url=https://soundcloud.com/zoeybloons/jump-to-lightspeed');
   table.boolean('is_master').notNullable().defaultTo(false);
   table.text('commit_comment').notNullable().defaultTo('Look at this.');
